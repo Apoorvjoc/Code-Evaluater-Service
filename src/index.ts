1e4e095924897c4a61025e7700b669c81c8b1408
Apoorvjoc <hlwapoorv@gmail.com>
2024-06-11 15:41:14 +0530

    intregated bull board ui

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import serverConfig from "./configs/server.config";
 import appRouter from "./routes";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import SampleWorker from "./workers/sampleWorker";
+import serverAdapter from "./configs/bullBoard.config"; 
 
 const app = express();
 
 app.use("/api" , appRouter);
+app.use("/ui" , serverAdapter.getRouter())
 
 app.listen(serverConfig.PORT , ()=>{
     console.log("Server started at...." , serverConfig.PORT);
+    console.log("bull board is running at :" , `http://localhost:${serverConfig.PORT}/ui`);
 
     SampleWorker('sampleQueue'); // creating worker (or consumer) which will listen to async call through queue
 
